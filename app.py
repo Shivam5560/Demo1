8d6ba859a91335f84d100447b3b83f7c4c16c32d
Shivam Sourav <59795223+Shivam5560@users.noreply.github.com>
2023-08-25 10:39:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from Code.face import find_faces
 from Code.encode import encoding_pictures
+import streamlit as st
 
 def main():
     encoding_pictures('Photos/')
     registration_id = find_faces('Videos/Video5.mp4')
     return registration_id
+
+print(st.text(main()))
